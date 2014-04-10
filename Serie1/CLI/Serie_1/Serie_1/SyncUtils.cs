abb79941211adc1c8cb5abc61f7e4e71f9aff017
Cátia Ormonde <a36923@alunos.isel.pt>
2014-04-10 14:46:21 +0100

    SyncUtils updated

--- Serie1/CLI/Serie_1/Serie_1/SyncUtils.cs
+++ Serie1/CLI/Serie_1/Serie_1/SyncUtils.cs
@@ -2,15 +2,28 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Serie_1
 {
     class SyncUtils
     {
-        public static int AdjustTimeout(ref int initialTime, ref int timeout)
+        public static int AdjustTimeout(ref int lastTime, ref int timeout)
         {
-            throw new NotImplementedException();
+            if (timeout != Timeout.Infinite)
+            {
+                int now = Environment.TickCount;
+                int elapsed = (now == lastTime) ? 1 : now - lastTime;
+                if (elapsed >= timeout)
+                    timeout = 0;
+                else
+                {
+                    timeout -= elapsed;
+                    lastTime = now;
+                }
+            }
+            return timeout;
         }
     }
 }
